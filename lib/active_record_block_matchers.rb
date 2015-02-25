f823df77e185f90ad972c4e1108dad2ffd687f36
Nathan Wallace <nathan.wallace@leandogsoftware.com>
2015-02-25 09:26:13 -0500

    remove dependencies on rspec-core and rspec-mocks

--- lib/active_record_block_matchers.rb
+++ lib/active_record_block_matchers.rb
@@ -1,5 +1,5 @@
 require "active_record_block_matchers/version"
-require "rspec"
+require "rspec/expectations"
 require "active_record"
 require "active_record_block_matchers/config"
 require "active_record_block_matchers/create_a_matcher"

867a6c22ac54391cf4b0ddddd3d31567054649fb
Nathan Wallace <nathan.m.wallace@gmail.com>
2015-10-20 13:08:56 -0400

    tidy up stylistic choices

--- lib/active_record_block_matchers/create_matcher.rb
+++ lib/active_record_block_matchers/create_matcher.rb
@@ -6,37 +6,34 @@ RSpec::Matchers.define :create do |klasses|
   description do
     "create #{klasses}"
   end
+
   match do |block|
     time_before = Time.current
 
     block.call
 
     @created_records = {}
-    klasses.each{
-        |klass, count|
+    klasses.each do |klass, count|
       column_name = ActiveRecordBlockMatchers::Config.created_at_column_name
       @created_records[klass] = klass.to_s.constantize.where("#{column_name} > ?", time_before)
-    }.select{
-      |klass, count|
+    end.select do |klass, count|
       count != @created_records[klass].count
-    }.empty?
+    end.empty?
   end
 
   failure_message do
-    generate_failure_message(klasses, 'should')
+    generate_failure_message(klasses, "should")
   end
 
   failure_message_when_negated do
-    generate_failure_message(klasses, 'should not')
+    generate_failure_message(klasses, "should not")
   end
 
   def generate_failure_message(klasses, should)
-    klasses.select{
-      |klass, count|
+    klasses.select do |klass, count|
       @created_records[klass] != count
-    }.map{
-        |klass, count|
+    end.map do |klass, count|
       "The block #{should} have created #{count} #{klass.to_s.pluralize(count)}, but created #{@created_records[klass].count}."
-    }.join(' ')
+    end.join(" ")
   end
 end
--- spec/create_records_matcher_spec.rb
+++ spec/create_records_matcher_spec.rb
@@ -19,13 +19,13 @@ RSpec.describe "`create` matcher" do
   it "fails when nothing is created" do
     expect {
       expect {}.to create(Person: 1)
-    }.to raise_error('The block should have created 1 Person, but created 0.')
+    }.to raise_error("The block should have created 1 Person, but created 0.")
   end
 
   it "fails when wrong number is created" do
     expect {
       expect { Person.create! }.to create(Person: 2)
-    }.to raise_error('The block should have created 2 People, but created 1.')
+    }.to raise_error("The block should have created 2 People, but created 1.")
   end
 
   it "fails with multiple errors wrong number is created" do
@@ -35,16 +35,16 @@ RSpec.describe "`create` matcher" do
         Person.create!
         Dog.create!
       }.to create(Person: 1, Dog: 2)
-    }.to raise_error('The block should have created 1 Person, but created 2. The block should have created 2 Dogs, but created 1.')
+    }.to raise_error("The block should have created 1 Person, but created 2. The block should have created 2 Dogs, but created 1.")
   end
 
-  it "passes the negative" do
-    expect { Person.create! }.to_not create(Person: 2)
+  it "can be negated" do
+    expect { Person.create! }.not_to create(Person: 2)
   end
 
   it "passes the negative" do
     expect {
-      expect { Person.create! }.to_not create(Person: 1)
-    }.to raise_error('The block should not have created 1 Person, but created 1.')
+      expect { Person.create! }.not_to create(Person: 1)
+    }.to raise_error("The block should not have created 1 Person, but created 1.")
   end
 end

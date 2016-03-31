e20318f9b60d2eab317c482a78fa69d63b5330d9
Nathan Wallace <nathan.m.wallace@gmail.com>
2016-03-31 17:37:30 -0400

    implement `with_attributes` add-on for `create_records` matcher

--- lib/active_record_block_matchers/create_records_matcher.rb
+++ lib/active_record_block_matchers/create_records_matcher.rb
@@ -8,26 +8,79 @@ RSpec::Matchers.define :create_records do |record_counts|
     "create #{counts_strs.join(", ")}"
   end
 
+  chain(:with_attributes) do |attributes|
+    # TODO: assert attributes has the right number of entries
+    @expected_attributes = attributes
+  end
+
   match do |block|
     time_before = Time.current
 
     block.call
 
-    @incorrect_record_counts =
-      record_counts.each_with_object({}) do |(klass, expected_count), incorrect_record_counts|
+    new_records =
+      record_counts.keys.each_with_object({}) do |klass, new_records|
         column_name = ActiveRecordBlockMatchers::Config.created_at_column_name
-        actual_count = klass.where("#{column_name} > ?", time_before).count
+        new_records[klass] = klass.where("#{column_name} > ?", time_before).to_a
+      end
+
+    @incorrect_counts =
+      new_records.each_with_object({}) do |(klass, new_records), incorrect|
+        actual_count = new_records.count
+        expected_count = record_counts[klass]
         if actual_count != expected_count
-          incorrect_record_counts[klass] = { expected: expected_count, actual: actual_count }
+          incorrect[klass] = { expected: expected_count, actual: actual_count }
         end
       end
-    @incorrect_record_counts.empty?
+    if @expected_attributes
+      @matched_records = Hash.new {|hash, key| hash[key] = []}
+      @all_attributes = Hash.new {|hash, key| hash[key] = []}
+      @incorrect_attributes =
+        @expected_attributes.each_with_object(Hash.new {|hash, key| hash[key] = []}) do |(klass, expected_attributes), incorrect|
+          @all_attributes[klass] = expected_attributes.map(&:keys).flatten.uniq
+          expected_attributes.each do |expected_attrs|
+            matched_record = (new_records.fetch(klass) - @matched_records[klass]).find do |record|
+              expected_attrs.all? {|k,v| values_match?(v, record.public_send(k))}
+            end
+            if matched_record
+              @matched_records[klass] << matched_record
+            else
+              incorrect[klass] << expected_attrs
+            end
+          end
+        end
+      @unmatched_records = @matched_records.map {|klass, records| [klass, new_records[klass] - records]}.to_h.reject {|k,v| v.empty?}
+    end
+    @incorrect_counts.blank? &&
+      @incorrect_attributes.blank?
   end
 
   failure_message do
-    @incorrect_record_counts.map do |klass, counts|
-      "The block should have created #{count_str(klass, counts[:expected])}, but created #{counts[:actual]}."
-    end.join(" ")
+    if @incorrect_counts.present?
+      @incorrect_counts.map do |klass, counts|
+        "The block should have created #{count_str(klass, counts[:expected])}, but created #{counts[:actual]}."
+      end.join(" ")
+    elsif @incorrect_attributes.present?
+      "The block should have created:\n" +
+        @expected_attributes.map do |klass, attrs|
+          "    #{attrs.count} #{klass} with these attributes:\n" +
+          attrs.map{|a| "        #{a.inspect}"}.join("\n")
+        end.join("\n") +
+        "\nDiff:" +
+        @incorrect_attributes.map do |klass, attrs|
+          "\n    Missing #{attrs.count} #{klass} with these attributes:\n" +
+          attrs.map{|a| "        #{a.inspect}"}.join("\n")
+        end.join("\n") +
+        @unmatched_records.map do |klass, records|
+          "\n    Extra #{records.count} #{klass} with these attributes:\n" +
+          records.map do |r|
+            attrs = @all_attributes[klass].each_with_object({}) {|attr, attrs| attrs[attr] = r.public_send(attr)}
+            "        #{attrs.inspect}"
+          end.join("\n")
+        end.join("\n")
+    else
+      "unknown error"
+    end
   end
 
   failure_message_when_negated do
--- spec/create_records_matcher_spec.rb
+++ spec/create_records_matcher_spec.rb
@@ -57,4 +57,76 @@ RSpec.describe "`create_records` matcher" do
   it "is aliased as `create`" do
     expect { Person.create! }.to create(Person => 1)
   end
+
+  it "can chain `with_attributes`" do
+    expect {
+      Person.create!(first_name: "Pam", last_name: "Greer")
+      Person.create!(first_name: "Bubba", last_name: "Conner")
+      Dog.create!(name: "Bucket", breed: "Terrier")
+    }.to create(Person => 2, Dog => 1)
+      .with_attributes(
+        Person => [
+          {first_name: "Pam", last_name: "Greer", full_name: "Pam Greer"},
+          {first_name: "Bubba", last_name: "Conner", full_name: "Bubba Conner"},
+        ],
+        Dog => [
+          {name: "Bucket", breed: "Terrier"}
+        ],
+      )
+  end
+
+  it "fails if expected attributes don't match" do
+    expect {
+      expect {
+        Person.create!(first_name: "Pam", last_name: "Morrison")
+        Person.create!(first_name: "Shay")
+        Person.create!(first_name: "Carter", last_name: "Townes")
+        Dog.create!(name: "Poppins")
+      }.to create(Person => 3, Dog => 1)
+        .with_attributes(Person => [
+          {first_name: "Pam", last_name: "Morrison"},
+          {first_name: "Boris"},
+          {first_name: "Hugh", last_name: "Townes"},
+        ],
+        Dog => [
+          {name: "Poppins"}
+        ])
+    }.to raise_error(%Q|The block should have created:
+    3 Person with these attributes:
+        {:first_name=>"Pam", :last_name=>"Morrison"}
+        {:first_name=>"Boris"}
+        {:first_name=>"Hugh", :last_name=>"Townes"}
+    1 Dog with these attributes:
+        {:name=>"Poppins"}
+Diff:
+    Missing 2 Person with these attributes:
+        {:first_name=>"Boris"}
+        {:first_name=>"Hugh", :last_name=>"Townes"}
+    Extra 2 Person with these attributes:
+        {:first_name=>"Shay", :last_name=>nil}
+        {:first_name=>"Carter", :last_name=>"Townes"}|)
+  end
+
+  it "allows you to not specify attributes for all record types" do
+    expect {
+      Person.create!(first_name: "Pam", last_name: "Morrison")
+      Dog.create!(name: "Poppins")
+    }.to create(Person => 1, Dog => 1)
+      .with_attributes(Dog => [{name: "Poppins"}])
+  end
+
+  it "doesn't care if the block creates unspecified records" do
+    expect {
+      Person.create!(first_name: "Pam", last_name: "Morrison")
+      Dog.create!(name: "Poppins")
+    }.to create(Dog => 1)
+      .with_attributes(Dog => [{name: "Poppins"}])
+  end
+
+  it "uses composable matchers for comparing attributes" do
+    expect {
+      Dog.create!(name: "Seymour")
+    }.to create(Dog => 1)
+      .with_attributes(Dog => [{name: a_string_starting_with("S")}])
+  end
 end
